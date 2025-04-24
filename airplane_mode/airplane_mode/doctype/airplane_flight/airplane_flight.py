5f8d1fec9822c97411538038cd4782fb8da3d96d
Aditya Singh <adityasingh14102001@gmail.com>
2025-04-24 16:26:30 +0530

    checkpoint: web-form

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -1,13 +1,20 @@
 # Copyright (c) 2025, Aditya and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.model.document import Document
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class AirplaneFlight(Document):
+class AirplaneFlight(WebsiteGenerator):
 	def on_submit(self):
 		self.update_flight_status()
 
 	def update_flight_status(self):
 		self.status = "Completed"
+
+	def fetch_airline_name(self):
+		if self.airplane:
+			airplane = frappe.get_doc("Airplane", self.airplane)
+			if airplane and airplane.airline:
+				return airplane.airline
+		return "N/A"
