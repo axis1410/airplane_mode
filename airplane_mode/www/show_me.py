0e3ac058ab91c3b773e98dc59811eb135302042c
Aditya Singh <adityasingh14102001@gmail.com>
2025-04-24 15:20:51 +0530

    checkpoint: show_me.html

--- airplane_mode/www/show_me.py
+++ airplane_mode/www/show_me.py
@@ -3,4 +3,8 @@ import frappe
 
 def get_context(context):
 	requested_color = frappe.form_dict.get("color")
+
 	context.color = requested_color if requested_color else "black"
+
+	context.title = "Color Display"
+	context.description = "A dynamic color display using Jinja templating"
