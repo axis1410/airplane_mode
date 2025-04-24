841d9482a4ec5bd9998fca3bd3835a648b3d43c3
Aditya Singh <adityasingh14102001@gmail.com>
2025-04-24 17:30:57 +0530

    checkpoint: submission day 2

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe.utils import format_date, format_duration
 from frappe.website.website_generator import WebsiteGenerator
 
 
@@ -13,8 +14,40 @@ class AirplaneFlight(WebsiteGenerator):
 		self.status = "Completed"
 
 	def fetch_airline_name(self):
-		if self.airplane:
-			airplane = frappe.get_doc("Airplane", self.airplane)
-			if airplane and airplane.airline:
-				return airplane.airline
+		try:
+			if self.airplane:
+				airplane = frappe.get_doc("Airplane", self.airplane)
+				if airplane and hasattr(airplane, "airline") and airplane.airline:
+					return airplane.airline
+		except Exception:
+			pass
 		return "N/A"
+
+	def get_context(self, context):
+		try:
+			context.title = self.name
+			context.doc = self
+
+			# Format duration properly using frappe utils
+			if hasattr(self, "duration") and self.duration:
+				context.formatted_duration = format_duration(self.duration)
+			else:
+				context.formatted_duration = "N/A"
+
+			# Format date properly
+			if hasattr(self, "date_of_departure") and self.date_of_departure:
+				context.formatted_date = format_date(self.date_of_departure, "d MMMM, YYYY")
+			else:
+				context.formatted_date = "N/A"
+
+			# Get airline info - can be accessed via doc in template
+			try:
+				context.airline_name = self.fetch_airline_name()
+			except Exception:
+				context.airline_name = "N/A"
+		except Exception as e:
+			frappe.log_error(
+				f"Error in AirplaneFlight.get_context: {str(e)}", "AirplaneFlight Template Error"
+			)
+
+		return context
